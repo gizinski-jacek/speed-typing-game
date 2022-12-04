236b0ccb1ef7e811c6f954dccb0706f63d486967
Jacek Gizinski <jacektrg+github@gmail.com>
2022-12-04 18:30:41 +0100

    Importing components, pipe and module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { TextInputDisplayPipe } from './pipes/text-input-display.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    TextInputDisplayPipe,
+  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
