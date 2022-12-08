6223b9d5307fe71ec72735ec32439aa35b54d9d2
Jacek Gizinski <jacektrg+github@gmail.com>
2022-12-08 21:10:19 +0100

    Adding header component.
    startCountdown subscribes to rsjx's timer which decrements countdown
    timer, when it reaches 0 calls startGameTimer handler function and
    emits start of game event to parent component.
    startGameTimer unsubscribes from countdown and subscribes to timer
    which decrements game time. On hitting 0 unsubscribes and emits end of
    game event to parent component.
    Adding reset handler function.

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,62 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Subscription, timer } from 'rxjs';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  @Output() start = new EventEmitter<any>();
+  @Output() end = new EventEmitter<any>();
+  countdownSub: Subscription | null = null;
+  countdown: number = 5;
+  gameStarted: boolean = false;
+  gameTimeSub: Subscription | null = null;
+  gameTime: number = 60;
+
+  ngOnInit() {}
+
+  ngOnDestroy() {
+    this.countdownSub?.unsubscribe();
+    this.gameTimeSub?.unsubscribe();
+  }
+
+  startCountdown(): void {
+    this.countdownSub = timer(0, 1000).subscribe(() => {
+      const newTime = this.countdown - 1;
+      if (this.gameStarted) {
+        this.startGameTimer();
+      }
+      if (newTime) {
+        this.countdown = newTime;
+      } else {
+        this.start.emit();
+        this.countdown = newTime;
+        this.gameStarted = true;
+      }
+    });
+  }
+
+  startGameTimer(): void {
+    this.countdownSub?.unsubscribe();
+    this.gameTimeSub = timer(0, 1000).subscribe(() => {
+      const newTime = this.gameTime - 1;
+      if (newTime) {
+        this.gameTime = newTime;
+      } else {
+        this.end.emit();
+        this.gameTimeSub?.unsubscribe();
+        this.gameTime = newTime;
+      }
+    });
+  }
+
+  reset(): void {
+    this.gameStarted = false;
+    this.countdown = 5;
+    this.gameTime = 60;
+    this.countdownSub?.unsubscribe();
+    this.gameTimeSub?.unsubscribe();
+  }
+}
