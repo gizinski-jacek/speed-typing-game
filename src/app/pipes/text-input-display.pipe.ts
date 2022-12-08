7e986ebfe7bff55f1c227707f19b55e66622577a
Jacek Gizinski <jacektrg+github@gmail.com>
2022-12-08 19:38:25 +0100

    Renaming paragraph to quote.
    Adding empty span tag to style it as keyboard cursor.

--- src/app/pipes/text-input-display.pipe.ts
+++ src/app/pipes/text-input-display.pipe.ts
@@ -5,35 +5,47 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TextInputDisplayPipe implements PipeTransform {
   transform(
-    paragraph: string,
+    quote: string,
     userInput: string
-  ): { text: string; input: string }[] | any {
-    const splitParagraph: string[] = [];
+  ): { quote: string; input: string }[] | any {
+    const splitQuote: string[] = [];
     const splitUserInput: string[] = [];
-    while (paragraph.length > 32) {
+    while (quote.length > 32) {
       let end: number = 0;
-      end = paragraph.substring(0, 32).lastIndexOf(' ');
-      splitParagraph.push(paragraph.substring(0, end));
-      paragraph = paragraph.substring(end);
+      end = quote.substring(0, 32).lastIndexOf(' ');
+      splitQuote.push(quote.substring(0, end));
+      quote = quote.substring(end);
       splitUserInput.push(userInput.substring(0, end));
       userInput = userInput.substring(end);
     }
-    splitParagraph.push(paragraph);
+    splitQuote.push(quote);
     splitUserInput.push(userInput);
-    const checkedInputs = splitUserInput.map((string, index) =>
+
+    let checkedInputs = splitUserInput.map((string, index) =>
       string
         .split('')
-        .map((letter, i) => {
-          if (letter === splitParagraph[index][i]) {
-            return `<span class="correct">${letter}</span>`;
+        .map((letter, i, array) => {
+          let html: string = '';
+          if (letter === splitQuote[index][i]) {
+            html = `<span class="correct">${letter}</span>`;
           } else {
-            return `<span class="wrong">${letter}</span>`;
+            html = `<span class="wrong">${letter}</span>`;
           }
+          if (!array.length) {
+            return '<span class="custom-cursor"></span>';
+          }
+          if (
+            i === array.length - 1 &&
+            array.length !== splitQuote[index].length
+          ) {
+            html = html + '<span class="custom-cursor"></span>';
+          }
+          return html;
         })
         .join('')
     );
-    const pairs = splitParagraph.map((string, index) => {
-      return { text: string, input: checkedInputs[index] };
+    const pairs = splitQuote.map((string, index) => {
+      return { quote: string, input: checkedInputs[index] };
     });
     return pairs;
   }
