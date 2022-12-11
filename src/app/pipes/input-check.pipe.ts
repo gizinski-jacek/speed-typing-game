82c52da93c35bb39bd018dcb7cf7d5b9c835c324
Jacek Gizinski <jacektrg+github@gmail.com>
2022-12-11 23:39:21 +0100

    Adding minor documentation.

--- src/app/pipes/input-check.pipe.ts
+++ src/app/pipes/input-check.pipe.ts
@@ -8,8 +8,11 @@ export class InputCheckPipe implements PipeTransform {
     quote: string,
     userInput: string
   ): { quote: string; input: string }[] {
+    // Splitting quote and user input into words arrays.
     const wordsInQuote: string[] = quote.split(' ');
-    const wordsInUserInput: string[] = userInput.split(' ');
+    const wordsInUserInput: string[] = userInput.split(' ').filter((w) => w);
+    // Checking user input words against quote words and applying "correct"
+    // or "wrong" class for styling purposes.
     const checkedWords = wordsInUserInput.map((word, index, array) => {
       let html: string = '';
       if (word === wordsInQuote[index]) {
@@ -25,12 +28,14 @@ export class InputCheckPipe implements PipeTransform {
       }
       return html;
     });
+
+    // Splitting words into 8 word long lines.
     const quoteLines: string[] = [];
-    const userInputLines: string[] = [];
     for (let i = 0; i < wordsInQuote.length; i += 8) {
       const chunk = wordsInQuote.slice(i, i + 8).join(' ');
       quoteLines.push(chunk);
     }
+    const userInputLines: string[] = [];
     for (let i = 0; i < checkedWords.length; i += 8) {
       const chunk = checkedWords.slice(i, i + 8).join(' ');
       userInputLines.push(chunk);
