b582e96bdd7a543c8bfef956a5b98dac65a1ee86
Jacek Gizinski <jacektrg+github@gmail.com>
2022-12-17 18:09:44 +0100

    Importing HeaderComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 import { RoundToDecimalPipe } from './pipes/round-to-decimal.pipe';
 import { InputCheckPipe } from './pipes/input-check.pipe';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { InputCheckPipe } from './pipes/input-check.pipe';
     MainComponent,
     RoundToDecimalPipe,
     InputCheckPipe,
+    HeaderComponent,
   ],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
